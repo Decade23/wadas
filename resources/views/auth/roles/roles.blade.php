24f336a9e9eaad47047b5755bbea9447929a1391
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2020-06-10 23:21:10 +0700

    add role product

--- resources/views/auth/roles/roles.blade.php
+++ resources/views/auth/roles/roles.blade.php
@@ -109,7 +109,39 @@
             </tr>
 
             <!-- Product -->
-            <!-- Product Group -->
+            <!-- Products -->
+            <tr>
+                <th scope="row">Product Groups</th>
+                <td class="text-center"">
+                <label class="kt-checkbox kt-checkbox--tick kt-checkbox--success">
+                    <input type="checkbox" class="acl" value="ok" name="product_create" {{ old('product_create') || array_key_exists('product.create', $permissions) ? 'checked' : ''}}>&nbsp;
+                    <span></span>
+                </label>
+                </td>
+                <td class="text-center">
+                    <label class="kt-checkbox kt-checkbox--tick kt-checkbox--success">
+                        <input type="checkbox" class="acl" value="ok" name="product_edit" {{ old('product_edit') || array_key_exists('product.edit', $permissions) ? 'checked' : ''}}>&nbsp;
+                        <span></span>
+                    </label>
+                </td>
+                <td class="text-center">
+                    <label class="kt-checkbox kt-checkbox--tick kt-checkbox--success">
+                        <input type="checkbox" class="acl" value="ok" name="product_show" {{ old('product_show') || array_key_exists('product.show', $permissions) ? 'checked' : ''}}>&nbsp;
+                        <span></span>
+                    </label>
+                </td>
+                <td class="text-center">
+                    <label class="kt-checkbox kt-checkbox--tick kt-checkbox--success">
+                        <input type="checkbox" class="acl" value="ok" name="product_destroy" {{ old('product_destroy') || array_key_exists('product.destroy', $permissions) ? 'checked' : ''}}> &nbsp;
+                        <span></span>
+                    </label>
+                </td>
+                <td class="text-center">
+                </td>
+            </tr>
+
+            <!-- Product -->
+            <!-- Product Groups -->
             <tr>
                 <th scope="row">Product Groups</th>
                 <td class="text-center"">
